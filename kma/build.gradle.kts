85edc7ccf3c3f6a559131a912bf56364e5d15395
lom <k.lomaha@tontilagunamobile.com>
2024-11-08 16:00:57 +0200

    Dependencies updated

--- kma/build.gradle.kts
+++ kma/build.gradle.kts
@@ -49,18 +49,18 @@ android {
 //apply(from = "$projectDir/gradle-mvn-publish.gradle")
 
 dependencies {
-    api("androidx.fragment:fragment-ktx:1.6.2")
+    api("androidx.fragment:fragment-ktx:1.8.5")
 
     val roomVersion = "2.6.1"
     implementation("androidx.room:room-runtime:$roomVersion")
     ksp("androidx.room:room-compiler:$roomVersion")
 
-    implementation("androidx.datastore:datastore-preferences:1.0.0")
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
 
     implementation("com.revenuecat.purchases:purchases:7.6.0")
 
     implementation("com.amplitude:analytics-android:1.12.2")
-    implementation("com.appsflyer:af-android-sdk:6.13.0")
+    implementation("com.appsflyer:af-android-sdk:6.15.2")
 
     implementation("com.google.android.play:app-update-ktx:2.1.0")
 
@@ -68,14 +68,14 @@ dependencies {
     implementation("com.google.firebase:firebase-config-ktx")
     implementation("com.google.firebase:firebase-analytics-ktx")
 
-    implementation("com.google.android.gms:play-services-ads-identifier:18.0.1")
-    implementation("com.google.android.gms:play-services-appset:16.0.2")
+    implementation("com.google.android.gms:play-services-ads-identifier:18.1.0")
+    implementation("com.google.android.gms:play-services-appset:16.1.0")
 
-    val moshiVersion = "1.14.0"
+    val moshiVersion = "1.15.0"
     implementation("com.squareup.moshi:moshi-kotlin:$moshiVersion")
     ksp("com.squareup.moshi:moshi-kotlin-codegen:$moshiVersion")
 
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
 
     implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
@@ -84,9 +84,9 @@ dependencies {
 
     implementation("com.android.installreferrer:installreferrer:2.2")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.7.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.8.1")
 
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
